bbca36b5a0dc49d7f205354c1fe03bc026f59fd0
eterna2 <eterna2@hotmail.com>
2015-09-19 00:44:39 +0800

    Updated bower distribution files

--- dist/csv-utility.js
+++ dist/csv-utility.js
@@ -1,4 +1,4 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.tmp = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.csvUtility = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports = {
   guessSeparator: guessSeparator,
   guessLinebreak: guessLinebreak,
@@ -6,16 +6,16 @@ module.exports = {
 };
 
 function guessCSV(line,opts){
-  var linebreak = guessLinebreak(line,opts);
-  var sep = guessSeparator(line,linebreak,opts);
+  opts = opts || {};
+  var linebreak = (Array.isArray(opts.linebreak))?guessLinebreak(line,opts.linebreak):opts.linebreak;
+  var sep = (Array.isArray(opts.sep))?guessSeparator(line,linebreak,opts.sep):opts.sep;
   return {sep:sep, linebreak:linebreak};
 }
 
 function guessSeparator(line,linebreak,opts){
   line = line.slice(0,102400);
   linebreak = linebreak || '\n';
-  opts = opts || {};
-  var choices = union([",", "\t", "|", ";"],opts.sep);
+  var choices = union([",", "\t", "|", ";"],opts);
 
   var res = choices[0], error = Number.MAX_VALUE;
   
@@ -50,8 +50,7 @@ function guessSeparator(line,linebreak,opts){
 
 function guessLinebreak(line,opts){
   line = line.slice(0,102400);
-  opts = opts || {};
-  var choices = union(["\r\n", "\r", "\n"],opts.linebreak);
+  var choices = union(["\r\n", "\r", "\n"],opts);
   var res = choices[0], score = 0;
   
   choices.forEach(function(sep,i){
